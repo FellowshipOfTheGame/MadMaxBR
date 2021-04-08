bdc898c4035e774d547079525ca320cf123bac42
pabloolvr <48020553+pabloolvr@users.noreply.github.com>
2021-04-08 16:35:16 -0300

    commit pablo

---
+++ Assets/Scripts/PowerUpScripts/PowerUpCollected.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpCollected : MonoBehaviour {
+
+    //[SerializeField] public int Color[5] = {"red",}; 
+    public GameObject PowerUp;
+    public GameObject PowerUpTrigger;
+    public GameObject Car;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    private void OnTriggerEnter() {
+        PowerUp.SetActive(false);
+        //gameObject.GetComponent<PowerUps>;
+        //var clr = Car.GetComponent<Renderer>();
+        //clr.material.SetColor("_Color", Color.red);
+    }
+}
