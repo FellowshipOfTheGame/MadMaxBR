bf3fcf6ac62b5ac0a3bc488e2f11b4708d2e185e
Pablo Oliveira <48020553+pabloolvr@users.noreply.github.com>
2021-11-23 20:43:13 -0300

    HUD Updated

--- Assets/Scripts/PowerUpScripts/Shield/ShieldPU.cs
+++ Assets/Scripts/PowerUpScripts/Shield/ShieldPU.cs
@@ -5,8 +5,11 @@ using System;
 
 public class ShieldPU : MonoBehaviour {
     private GameObject targetCar;
+    // the hud shield bar
+    public GameObject ShieldBar;
     private void Update() {
         if (targetCar.GetComponent<VehicleData>().GetCurrentShield() <= 0) {
+            ShieldBar.SetActive(false);
             // unequip powerup
             this.gameObject.transform.parent.GetComponentInParent<VehicleData>().EmptyPowerUpSlot(PowerUpName.Shield);
             // set powerup gameObject inactive
@@ -16,6 +19,7 @@ public class ShieldPU : MonoBehaviour {
 
     public void Activate() {
         targetCar = this.transform.parent.gameObject.transform.parent.gameObject; // get the car this script is attached to
-        targetCar.GetComponent<VehicleData>().AddShield(200);
+        ShieldBar.SetActive(true);
+        targetCar.GetComponent<VehicleData>().AddShield(10);
     }
 }
