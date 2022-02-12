6e9aae96d2e7e63985753e271b8680ea8c6e5fd6
Igor <51010839+Sfrev@users.noreply.github.com>
2022-02-12 20:38:42 -0300

    Wrong way warning when player is backtracking

--- Assets/Scripts/WrongWay.cs
+++ Assets/Scripts/WrongWay.cs
@@ -6,15 +6,35 @@ public class WrongWay : MonoBehaviour
     [SerializeField] private Image wrongWayImage;
     private GameObject _playerCar;
     private float _time;
+    private CarMovementAI _carMovementAI;
     
     private void Awake()
     {
         _playerCar = FindObjectOfType<WrongWay>().gameObject;
+        _carMovementAI = _playerCar.GetComponent<CarMovementAI>();
+        ColorAlfaZero();
     }
 
     private void Update()
     {
+        if (Vector3.Dot(_carMovementAI.nodes[_carMovementAI.currentNode].transform.right, gameObject.transform.forward) < -0.3)
+        {
+            FadeInOut();
+        }
+        else
+        {
+            ColorAlfaZero();
+        }
+    }
+
+    private void FadeInOut()
+    {
         _time += Time.deltaTime * 2;
-        wrongWayImage.color =  new Color(1, 1, 1,(Mathf.Sin(_time) + 1) * 0.5f);
+        wrongWayImage.color = new Color(1, 1, 1, (Mathf.Sin(_time) + 1) * 0.5f);
+    }
+
+    private void ColorAlfaZero()
+    {
+        wrongWayImage.color =  new Color(1, 1, 1, 0);
     }
 }
