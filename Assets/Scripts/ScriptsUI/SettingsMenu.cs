9475ed0a4f10d8ea21ca8cfc3560f90b85db471e
Amauri Antonio de Oliveira <amauriibate32@hotmail.com>
2022-02-26 17:02:23 -0300

    Melhora no sistema de load e salve

--- Assets/Scripts/ScriptsUI/SettingsMenu.cs
+++ Assets/Scripts/ScriptsUI/SettingsMenu.cs
@@ -8,8 +8,12 @@ public class SettingsMenu : MonoBehaviour
 {
     private Resolution[] resolutions;
 
-    public Dropdown resolutionsDropdown;
-    public Dropdown qualityDropdown;
+    [SerializeField] private Dropdown resolutionsDropdown;
+    [SerializeField] private Dropdown qualityDropdown;
+    [SerializeField] private Toggle isFullScreenToggle;
+    [SerializeField] private Slider mainSlider;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider effectdSlider;
 
     public AudioMixer mainAudioMixer;
 
@@ -18,33 +22,26 @@ public class SettingsMenu : MonoBehaviour
     private float musicVolume = 0;
 
     private bool isFullscreen = true;
-    private int qualityLevel = 3;
+    private int qualityLevel = 2;
     private int resolutionLevel = 0;
 
 
     private void Awake()
     {
-        if (PlayerPrefs.GetFloat("mainVolume") != 0)
-        {
-            mainVolume = PlayerPrefs.GetFloat("mainVolume");
-        }
-        if (PlayerPrefs.GetFloat("effectVolume") != 0)
-        {
-            effectVolume = PlayerPrefs.GetFloat("effectVolume");
-        }
-        if (PlayerPrefs.GetFloat("musicVolume") != 0)
-        {
-            musicVolume = PlayerPrefs.GetFloat("musicVolume");
-        }
+        mainVolume = PlayerPrefs.HasKey("mainVolume") ? PlayerPrefs.GetFloat("mainVolume") : 0;
+        effectVolume = PlayerPrefs.HasKey("effectVolume") ? PlayerPrefs.GetFloat("effectVolume") : 0;
+        musicVolume = PlayerPrefs.HasKey("musicVolume") ? PlayerPrefs.GetFloat("musicVolume") : 0;
 
-        //   isFullscreen = PlayerPrefs.GetBool
-        if (PlayerPrefs.GetInt("qualityLevel") != 0)
+        qualityLevel = PlayerPrefs.HasKey("qualityLevel") ? PlayerPrefs.GetInt("qualityLevel") : 2;
+        resolutionLevel = PlayerPrefs.HasKey("resolutionLevel") ? PlayerPrefs.GetInt("resolutionLevel") : 0;
+
+        if (PlayerPrefs.GetString("isFullscreen") == "True")
         {
-            qualityLevel = PlayerPrefs.GetInt("qualityLevel");
+            isFullscreen = true;
         }
-        if (PlayerPrefs.GetInt("resolutionLevel") != 0)
+        else
         {
-            resolutionLevel = PlayerPrefs.GetInt("resolutionLevel");
+            isFullscreen = false;
         }
     }
 
@@ -56,55 +53,69 @@ public class SettingsMenu : MonoBehaviour
         resolutionsDropdown.ClearOptions();
         List<string> options = new List<string>();
 
-
         for (int i = 0; i < resolutions.Length; i++)
         {
             options.Add(resolutions[i].width + " x " + resolutions[i].height);
-
             if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
             {
-                resolutionLevel = i;
+                if (resolutionLevel == 0)
+                {
+                    resolutionLevel = i;
+                }
             }
         }
 
         resolutionsDropdown.AddOptions(options);
-        resolutionsDropdown.value = resolutionLevel;
         resolutionsDropdown.RefreshShownValue();
+        resolutionsDropdown.value = resolutionLevel;
+        SetResolution(resolutionLevel);
 
+        isFullScreenToggle.isOn = isFullscreen;
+        SetFullScreen(isFullscreen);
 
-        qualityDropdown.value = qualityLevel;
         qualityDropdown.RefreshShownValue();
+        qualityDropdown.value = qualityLevel;
+
+        mainSlider.value = mainVolume;
+        musicSlider.value = musicVolume;
+        effectdSlider.value = effectVolume;
     }
 
 
     public void SetMainVolume(float volume)
     {
         mainAudioMixer.SetFloat("MainAudio", volume);
+        PlayerPrefs.SetFloat("mainVolume", volume);
     }
 
     public void SetMusicVolume(float volume)
     {
         mainAudioMixer.SetFloat("MusicAudio", volume);
+        PlayerPrefs.SetFloat("musicVolume", volume);
     }
 
     public void SetEffectVolume(float volume)
     {
         mainAudioMixer.SetFloat("EffectAudio", volume);
+        PlayerPrefs.SetFloat("effectVolume", volume);
     }
 
     public void SetGraphicQuality(int quantityIndex)
     {
         QualitySettings.SetQualityLevel(quantityIndex);
+        PlayerPrefs.SetInt("qualityLevel", quantityIndex);
     }
 
-    public void SetFullScreen(bool isFullScreen)
+    public void SetFullScreen(bool isFullScreenMode)
     {
-        Screen.fullScreen = isFullScreen;
+        Screen.fullScreen = isFullScreenMode;
+        PlayerPrefs.SetString("isFullScreen", isFullScreenMode ? "True" : "False");
     }
 
     public void SetResolution(int resolutionIndex)
     {
         Resolution resolution = resolutions[resolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        PlayerPrefs.SetInt("resolutionLevel", resolutionIndex);
     }
 }
--- Assets/Scripts/SpawnCar.cs
+++ Assets/Scripts/SpawnCar.cs
@@ -6,44 +6,13 @@ using System;
 
 public class SpawnCar : MonoBehaviour
 {
-    [SerializeField] public int selectedId;
-    [SerializeField] public int selectedIdColor;
-    public GameObject playerSpawnPoint;
-    private MeshRenderer m_Renderer;
-    private RaceManager raceManager;
-    public Carro[] cars;
+    [SerializeField] private CarName carName;
+    [SerializeField] private CarColor carColor;
 
     private void Awake()
     {
-        selectedId = PlayerPrefs.GetInt("selectedId");
-        selectedIdColor = PlayerPrefs.GetInt("selectedIdColor");
-        cars[selectedId].car.SetActive(true);
-        // GameObject player  = Instantiate(cars[selectedId].car, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
-        SetCarColor();
-        Debug.Log("Passou");
-        cars[selectedId].car.transform.position = new Vector3(playerSpawnPoint.transform.position.x, playerSpawnPoint.transform.position.y, playerSpawnPoint.transform.position.z);
-        RaceManager.Instance.Player = cars[selectedId].car;
-        RaceManager.Instance.Racers[RaceManager.Instance.Racers.Count - 1] = cars[selectedId].car;
-        for (int i = 0; i < cars.Length; i++)
-        {
-            //if (i != selectedId)
-            //{
-               // GameObject.Destroy
-                Debug.Log(cars[i].car.name);
-            //}
-        }
-    }
-
-    private void SetCarColor()
-    {
-        m_Renderer = cars[selectedId].carBody.GetComponent<MeshRenderer>();
-        for (int i = 0; i < m_Renderer.sharedMaterials.Length; i++)
-        {
-            //           Debug.Log(m_Renderer.sharedMaterials[i].name);
-            //          if (m_Renderer.sharedMaterials[i].name == cars[selectedId].carBodyName)
-            //           {
-            m_Renderer.sharedMaterials[i].mainTexture = cars[selectedId].materials[selectedIdColor];
-            //        }
-        }
+        carName = (CarName) PlayerPrefs.GetInt("selectedId");
+        carColor = (CarColor) PlayerPrefs.GetInt("selectedIdColor");
+        RaceManager.Instance.SpawnPlayer(null, carName, carColor, "Jurema");
     }
 }
